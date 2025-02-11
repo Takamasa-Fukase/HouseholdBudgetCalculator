4b8f186b19a9c8f6062f3639f1f646b80abd0a3c
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-11 13:03:24 +0900

    Initial Commit

---
+++ HouseholdBudgetCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HouseholdBudgetCalculator
+//
+//  Created by ウルトラ深瀬 on 11/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
