5c9fe1d0e3b568668777c42601873712a895b0df
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-11 18:26:35 +0900

    旅行費用計算アプリから金額入力画面を移植

---
+++ HouseholdBudgetCalculator/DateDetail/PaymentListItemCell.swift
@@ -0,0 +1,44 @@
+//
+//  PaymentListItemCell.swift
+//  TravelBudgetCalculator
+//
+//  Created by ウルトラ深瀬 on 30/8/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class PaymentListItemCell: UITableViewCell {    
+    var disposeBag = DisposeBag()
+    var id: UUID = UUID()
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var yenDisplayLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupTitleTextField()
+        setupAmountTextField()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+
+    func setupTitleTextField() {
+        titleTextField.returnKeyType = .done
+    }
+    
+    func setupAmountTextField() {
+        amountTextField.keyboardType = .numbersAndPunctuation
+        amountTextField.returnKeyType = .done
+        amountTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 0))
+        amountTextField.rightViewMode = .always
+    }
+}
