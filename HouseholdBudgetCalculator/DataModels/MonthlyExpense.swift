38f58031d67d75a0ba2a6eb13fc1a0778139aa63
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-11 23:31:02 +0900

    no message

--- HouseholdBudgetCalculator/DataModels/MonthlyExpense.swift
+++ HouseholdBudgetCalculator/DataModels/MonthlyExpense.swift
@@ -8,19 +8,37 @@
 import Foundation
 
 struct MonthlyExpense: Codable {
-    let id: UUID = UUID()
+    let id: UUID
     let title: String
     var expenseGroups: [ExpenseGroup]
+    
+    init(title: String, expenseGroups: [ExpenseGroup]) {
+        self.id = UUID()
+        self.title = title
+        self.expenseGroups = expenseGroups
+    }
 }
 
 struct ExpenseGroup: Codable {
-    let id: UUID = UUID()
+    let id: UUID
     let title: String
     var items: [ExpenseItem]
+    
+    init(title: String, items: [ExpenseItem]) {
+        self.id = UUID()
+        self.title = title
+        self.items = items
+    }
 }
 
 struct ExpenseItem: Codable {
-    let id: UUID = UUID()
+    let id: UUID
     var title: String
     var amount: Int
+    
+    init(title: String, amount: Int) {
+        self.id = UUID()
+        self.title = title
+        self.amount = amount
+    }
 }
