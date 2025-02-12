2b014f63a0bdf889ba67119fb216c3eb9e8cc29f
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-12 14:23:23 +0900

    ディレクトリ移動と、StoryboardからXibへの差し替えなど

--- HouseholdBudgetCalculator/Presentation/ExpenseInput/ExpenseInputViewController.swift
+++ HouseholdBudgetCalculator/Presentation/ExpenseInput/ExpenseInputViewController.swift
@@ -11,12 +11,18 @@ import RxCocoa
 
 class ExpenseInputViewController: UIViewController {
     var activeTextField: UIView?
-    var monthlyExpense: MonthlyExpense = .init(
-        title: "",
-        expenseGroups: []
-    )
+    let expenseGroup: ExpenseGroup
 
-    @IBOutlet weak var tableView: TouchesBeganTableView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    init(expenseGroup: ExpenseGroup) {
+        self.expenseGroup = expenseGroup
+        super.init(nibName: MonthListViewController.className, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
