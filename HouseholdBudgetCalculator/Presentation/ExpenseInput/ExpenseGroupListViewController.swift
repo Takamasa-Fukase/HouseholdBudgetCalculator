defd2127f9e4c6cb22d976f0387856244af4774e
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-12 14:24:05 +0900

    支出グループごとに横スクロール表示するためにParchmentの設定を追加

--- HouseholdBudgetCalculator/Presentation/ExpenseInput/ExpenseGroupListViewController.swift
+++ HouseholdBudgetCalculator/Presentation/ExpenseInput/ExpenseGroupListViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 import Parchment
 
 class ExpenseGroupListViewController: UIViewController {
+    var pagingViewController: PagingViewController!
+    // TODO: initでDIしてletにしたい
     var monthlyExpense: MonthlyExpense = .init(title: "", expenseGroups: [])
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = monthlyExpense.title
         view.backgroundColor = .systemBackground
+        setupParchment()
+    }
+    
+    private func setupParchment() {
+        let vcs = monthlyExpense.expenseGroups.map { expenseGroup in
+            let vc = ExpenseInputViewController(expenseGroup: expenseGroup)
+            vc.title = expenseGroup.title
+            return vc
+        }
+        
+        pagingViewController = .init(viewControllers: vcs)
+        pagingViewController.textColor = .systemGray
+        pagingViewController.selectedTextColor = .label
+        pagingViewController.menuBackgroundColor = .systemBackground
+        pagingViewController.font = .systemFont(ofSize: 14, weight: .medium)
+        pagingViewController.selectedFont = .systemFont(ofSize: 14, weight: .medium)
+        pagingViewController.indicatorOptions = .visible(height: 4, zIndex: .max - 1, spacing: .init(top: 0, left: 12, bottom: 0, right: 12), insets: .zero)
+        
+        // MARK: 一番入力済み項目の件数が多い（よく使う）グループを最初に表示する
+        // まず金額が入力済みの項目があるグループだけにフィルタリング
+        let nonEmptyExpenseGroups = monthlyExpense.expenseGroups.filter { $0.items.contains(where: { $0.amount > 0 }) }
+        
+        // それをitemsの件数が多い順にソートし、その1番目のグループを取得
+        let mostUsedExpenseGroup = nonEmptyExpenseGroups.sorted(by: { $0.items.count > $1.items.count }).first
+
+        // そのグループが元の配列の中で何番目にあるかを取得
+        let mostUsedExpenseGroupIndex = monthlyExpense.expenseGroups.firstIndex(where: { $0.id == mostUsedExpenseGroup?.id }) ?? 0
+        
+        // 最初に表示するページを設定
+        pagingViewController.select(index: mostUsedExpenseGroupIndex)
+        
+        addChild(pagingViewController)
+        view.addSubview(pagingViewController.view)
+        pagingViewController.didMove(toParent: self)
+        pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        pagingViewController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        pagingViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        pagingViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        pagingViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
 }
