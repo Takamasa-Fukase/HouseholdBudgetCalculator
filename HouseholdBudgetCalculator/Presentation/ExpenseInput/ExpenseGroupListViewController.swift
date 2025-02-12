cb0bbb04dab73569dda582b5990cfb5735f2f30e
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-12 10:18:56 +0900

    ディレクトリ整理

---
+++ HouseholdBudgetCalculator/Presentation/ExpenseInput/ExpenseGroupListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ExpenseGroupListViewController.swift
+//  HouseholdBudgetCalculator
+//
+//  Created by ウルトラ深瀬 on 12/2/25.
+//
+
+import UIKit
+import Parchment
+
+class ExpenseGroupListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
