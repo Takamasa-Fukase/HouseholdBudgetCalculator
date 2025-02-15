26dcedc11424c3f2a81f257c9e26381b8245d85b
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-15 11:46:54 +0900

    月の一覧を横スワイプで削除

--- HouseholdBudgetCalculator/Presentation/MonthList/MonthListViewController.swift
+++ HouseholdBudgetCalculator/Presentation/MonthList/MonthListViewController.swift
@@ -103,6 +103,17 @@ extension MonthListViewController: UITableViewDelegate, UITableViewDataSource {
         vc.monthlyExpense = UserDefaults.monthlyExpenses[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            UserDefaults.monthlyExpenses.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
 
 extension MonthListViewController: UIDocumentPickerDelegate {
